1c07a47db6111df4a361720d647a0cebd5a0040c
naveen_alex <naveenalex512@gmail.com>
2025-06-13 12:01:47 +0530

    ini

---
+++ app.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import dataSource from "./db/data-source";
+
+import { errorMiddleware } from "./middlewares/errorMiddleware";
+import {authRouter} from "./routes/auth.route"
+
+import cors from "cors";
+
+const server = express();
+
+server.use(cors())
+server.use(express.json());
+
+
+server.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello world");
+});
+
+
+server.use("/auth",authRouter);
+
+server.use(errorMiddleware);
+
+(async()=>{
+  try{
+    await dataSource.initialize();
+    console.log('Database connected');
+  }catch (e){
+    console.error(`Failed to connect to DB - ${e}`)
+    process.exit(1);
+  }
+  server.listen(3000, () => {
+    console.info("server listening to 3000");
+  });
+})();
+
+
