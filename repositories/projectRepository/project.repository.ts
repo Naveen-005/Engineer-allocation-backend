19f3669cb7c1eb902b5ebe00525b4bc745cbc868
Riya Sunil <riaelisabeths@gmail.com>
2025-06-16 00:12:08 +0530

    endpoint : get, update, delete project

--- repositories/projectRepository/project.repository.ts
+++ repositories/projectRepository/project.repository.ts
@@ -2,11 +2,47 @@ import { In, Repository } from "typeorm";
 import { Project } from "../../entities/projectEntities/project.entity";
 
 class ProjectRepository {
-constructor(private repository : Repository<Project> ){}
-    async create(project: Project) : Promise<Project> {
-        return this.repository.save(project)
-    }
+  constructor(private repository: Repository<Project>) {}
+  async create(project: Project): Promise<Project> {
+    return this.repository.save(project);
+  }
 
+  async findMany(): Promise<Project[]> {
+    return this.repository.find({
+      relations: {
+        pm: true,
+        lead: true,
+        projectUsers: { user: true },
+        notes: { author: true },
+        requirements: true,
+      },
+    });
+  }
+
+  async findOneById(id: number): Promise<Project> {
+    return this.repository.findOne({
+      where: { id },
+      relations: {
+        pm: true,
+        lead: true,
+        projectUsers: { user: true },
+        notes: { author: true },
+        requirements: true,
+      },
+    });
+  }
+
+  async update(id: number, project: Project) {
+    await this.repository.save({ id, ...project });
+  }
+
+  async delete(id: number) {
+    await this.repository.delete(id);
+  }
+
+  async remove(project: Project) {
+    await this.repository.softRemove(project);
+  }
 }
 
-export default ProjectRepository
+export default ProjectRepository;
