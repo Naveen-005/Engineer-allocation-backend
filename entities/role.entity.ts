b6cf7643e2989e3e47bde43f4afc5fbf2da8ddf0
Gus <gourisankar.shaji@gmail.com>
2025-06-16 23:00:10 +0530

    added constructors for some entities and created a common types file

--- entities/role.entity.ts
+++ entities/role.entity.ts
@@ -1,15 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { User } from './userEntities/user.entity';
-import AbstractEntity from './abstract.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { User } from "./userEntities/user.entity";
+import AbstractEntity from "./abstract.entity";
 
-@Entity('roles')
+@Entity("roles")
 export class Role extends AbstractEntity {
+  constructor(role_name?: string) {
+    super();
+    if (role_name) this.role_name = role_name;
+  }
+
   @PrimaryGeneratedColumn()
   role_id: number;
 
   @Column({ unique: true })
   role_name: string;
 
-  @OneToMany(() => User, user => user.role)
+  @OneToMany(() => User, (user) => user.role)
   users: User[];
 }
